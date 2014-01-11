e0fefa56afd58b31118ad26e7f8ac42ce28953d9
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2014-01-11 18:57:15 +0200

    Added code for setting the activity up to use the login fragment.

--- src/com/relaxisapp/relaxis/MainActivity.java
+++ src/com/relaxisapp/relaxis/MainActivity.java
@@ -1,15 +1,29 @@
 package com.relaxisapp.relaxis;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
+	private MainFragment mainFragment;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		if (savedInstanceState == null) {
+			// Add the fragment on initial activity setup
+	        mainFragment = new MainFragment();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+		} else {
+			// Or set the fragment from restored state info
+	        mainFragment = (MainFragment) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+		}
 	}
 
 	@Override
